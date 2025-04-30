e2adc0951c0f2bb7c85bfa6d25a065062aaff172
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-04-30 17:13:59 -0400

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Kiara on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubCollaborationTests/GitHubCollaborationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationTests.swift
+//  GitHubCollaborationTests
+//
+//  Created by Kiara on 4/30/25.
+//
+
+import Testing
+@testable import GitHubCollaboration
+
+struct GitHubCollaborationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
